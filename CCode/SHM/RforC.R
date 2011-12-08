bf6bd46b276d22f8dbfb103216d49ef2de8a7722
Jonathan Heydari <j.j.heydari@ncl.ac.uk>
2011-12-08 21:03:08 +0000

    jh

--- CCode/SHM/RforC.R
+++ CCode/SHM/RforC.R
@@ -1,14 +1,14 @@
-library(qfa,lib="~/R")
+library(qfa)
 
- library(qfaBayes,lib="~/R")
+ library(qfaBayes)
 
 
 
  library(qfaBayes)
 
-Control<-c("Adam_cdc13-1_SDLV2_REP1.txt")
-DescripControl<-"ExptDescriptionCDC13.txt"
-a<-rod.read(files=Control[1],inoctimes=DescripControl)
+Control<-c("cdc13-1_rad9D_SDLv2_Rpt1.txt","cdc13-1_rad9D_SDLv2_Rpt2.txt","cdc13-1_rad9D_SDLv2_Rpt3.txt","cdc13-1_rad9D_SDLv2_Rpt4.txt")
+DescripControl<-"ExptDescriptionCDC13RAD9.txt"
+a<-rod.read(files=Control,inoctimes=DescripControl)
 
 Row<-paste(a$Row)
 Col<-paste(a$Col)
@@ -16,18 +16,33 @@ for (i in 1:nrow(a)){
 if (nchar(Row[i])<2){Row[i]=paste(0,Row[i],sep="")}
 if (nchar(Col[i])<2){Col[i]=paste(0,Col[i],sep="")}
 }
+
 a$ID<-paste(a$Barcode,a$MasterPlate.Number,Row,Col,sep="")
+
+ORFuni=unique(a$ORF)[order(unique(a$ORF))]########
+funcIDlist<-function(x){
+a$ID[a$ORF==x]
+}
+funcStrip<-function(x,i){x[1:i]}
+IDstrip=sapply(ORFuni,funcIDlist)
+IDstrip=sapply(IDstrip,unique)
+IDstrip=lapply(IDstrip,i=6,funcStrip)
+IDstrip=unlist(IDstrip)
+IDstrip=na.omit(IDstrip)
+a<-a[a$ID%in%IDstrip,]
+#########
+
 a<-a[order(a$ORF,a$ID,a$Expt.Time), ]
 
 
 qfa.variables(a)
 Screen<-as.character(unique(a$Screen.Name))
 Treat<-27
-MPlate<-15
+MPlate<-as.character(unique(a$MasterPlate.Number))
 a<-funcREMOVE(a,Screen,Treat,MPlate)
 
 
-Scaling=FALSE
+Scaling=TRUE
 IDuni<-unique(a$ID)
 ORFuni<-unique(a$ORF)
 
@@ -40,7 +55,7 @@ NoSum_a<-c(0,unlist(lapply(1:N,funcNoSum,NoORF_vec=NoORF_a)))
 dimr<-max(NoORF_a);dimc<-max(NoTime_a)
 y<-funcXY(a$Growth,M,N,NoTime_a,NoSum_a,dimr,dimc)
 x<-funcXY(a$Expt.Time,M,N,NoTime_a,NoSum_a,dimr,dimc)
-y<-y/max(y[!is.na(y)])###
+
 
 QFA.I<-list("NoORF"=c(NoORF_a),"NoTime"=c(NoTime_a)[-1],"NoSum"=c(NoSum_a),"N"=
 N,"M"=M,"gene"=gene)
@@ -56,3 +71,6 @@ write.table(file="ydata.txt",c(yy))
 
 write.table(file="NoORFdata.txt",c(NoORF_a))
 write.table(file="NoTIMEdata.txt",c(NoTime_a)[-1])
+write.table(file="LMNmaxdata.txt",c(N,max(NoORF_a),max(NoTime_a)))
+
+
