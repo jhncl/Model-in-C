b255dd5e3b93d42b82c552f7f5a84b6859ce6fe4
Jonathan Heydari <j.j.heydari@ncl.ac.uk>
2011-12-12 22:44:08 +0000

    jh

---
+++ CCode/SHM-g/datain.c
@@ -0,0 +1,247 @@
+#include "headers.h"
+#include "datain.h"
+
+
+/*TEST*/
+
+int testargc(int argc)
+{
+ 	if (argc!=4) {
+    		perror("argc failed");
+    		exit(EXIT_FAILURE);
+  	}
+return 0;
+}
+
+int testsame(int a,int b)
+{
+ 	if (a!=b) {
+    		perror("data int failed");
+    		exit(EXIT_FAILURE);
+  	}
+return 0;
+}
+
+
+
+/*READ IN*/
+
+
+int datadouble(char filename[], double datavec[],int length )
+{
+	int i;
+	char number[20];
+	double data;
+	FILE *file;
+	file=fopen(filename, "r");
+	i=0;
+	if ( file != NULL ){
+		fscanf(file, "%s %lf",number,&data);
+		while ( fscanf(file, "%s %lf",number,&data)!=-1){
+			datavec[i]=data;   
+			i++;
+		}
+	}
+	else{perror(filename);}
+	testsame(length,i);
+	fclose(file);
+return 0;
+}
+
+int dataint(char filename[], int datavec[], int length)
+{
+	int i;
+	char number[20];
+	double data;
+	FILE *file;
+	file = fopen(filename, "r");
+	i=0;
+	if ( file != NULL ){
+		fscanf(file, "%s %lf",number,&data);
+		while (fscanf(file, "%s %lf",number,&data)!=-1){
+			datavec[i]=data;
+			i++;
+        	}
+    	}
+	else{perror(filename);}
+	testsame(length,i);
+	fclose(file);
+return 0;
+}
+
+int dataLMN(char filename[], int *datavecL,int *datavecM,int *datavecN,int *datavecmaxy,int *datavecmaxTIME)
+{
+	char number[20];
+	double data;
+	FILE *file = fopen(filename, "r");
+	if ( file != NULL ){
+		fscanf(file, "%s %lf",number,&data);
+		fscanf(file, "%s %lf",number,&data);
+		*datavecL=data;     
+		fscanf(file, "%s %lf",number,&data);
+		*datavecM=data;
+		fscanf(file, "%s %lf",number,&data);
+		*datavecN=data;
+		fscanf(file, "%s %lf",number,&data);
+		*datavecmaxy=data;
+		fscanf(file, "%s %lf",number,&data);
+		*datavecmaxTIME=data;
+	}
+	else{perror(filename);}
+	fclose(file);
+return 0;
+}
+
+/*INZ*/
+
+int inzstruct_MH(struct_MH *MH)
+{
+	fillMH(MH);
+return 0;
+}
+
+int inzstruct_priors(struct_priors *priors)
+{
+	fillpriors(priors);
+return 0;
+}
+
+int inzstruct_data(struct_data *data)
+{
+	long size;
+	dataLMN("LMNmaxdata.txt",&data->L,&data->M,&data->N,&data->maxy,&data->maxNoTIME);  
+	testsame(data->L*data->M*data->N,data->maxy);
+
+	size=data->L*data->M*data->N; /*input from file*/ 
+  	data->y=malloc(size*sizeof(double));        /*Cycle with SHIFTlmn*/
+        data->x=malloc(size*sizeof(double));        /*Cycle with SHIFTlmn*/
+	size=data->L;
+	data->NoORF=malloc(size*sizeof(double));    /*Cycle with data->L*/
+	data->NoSUM=malloc(size*sizeof(double));    /*Cycle with data->L*/
+
+	/*if (data->y==NULL||data->x==NULL||data->NoORF==NULL||data->NoSUM==NULL||data->NoTIME==NULL) {
+		perror("malloc failed");
+    		exit(EXIT_FAILURE);
+  	}*/
+
+	datadouble("ydata.txt",data->y,data->L*data->M*data->N);
+        datadouble("xdata.txt",data->x,data->L*data->M*data->N);
+        dataint("NoORFdata.txt",data->NoORF,data->L);
+
+	filldata(data);
+	testsame(data->maxNoTIME,data->SHIFTlmn);
+
+	size=data->SHIFTlmn;/*inputfromfile*/
+	data->NoTIME=malloc(size*sizeof(double));   /*Cycle with SHIFTlm*/
+        dataint("NoTIMEdata.txt",data->NoTIME,data->SHIFTlmn);
+
+
+return 0;
+}
+
+int inzstruct_para(struct_para *para,struct_data *data)
+{
+	long size;
+
+	size=data->SHIFTlmn;
+	para->K_lm=malloc(size*sizeof(double));
+	para->r_lm=malloc(size*sizeof(double));
+	size=data->L;
+	para->tau_K_l=malloc(size*sizeof(double));
+	para->tau_r_l=malloc(size*sizeof(double));
+	para->K_o_l=malloc(size*sizeof(double));
+	para->r_o_l=malloc(size*sizeof(double));
+	para->nu_l=malloc(size*sizeof(double));
+
+	fillpara(para,data);
+return 0;
+}
+
+/*FILL*/
+
+int fillMH(struct_MH *MH)
+{
+	MH->hK=0.1;	MH->accept_K=0;
+	MH->hr=0.1;	MH->accept_r=0;
+	MH->hnu=0.1;	MH->accept_nu=0;
+	MH->hP=0.2;	MH->accept_P=0;  /*h sd; accept=0*/
+return 0;
+}
+
+int filldata(struct_data *D)
+{
+	int l;
+
+	D->NoSUM[0]=0;
+	for (l=1;l<(D->L);l++){
+		D->NoSUM[l]=D->NoSUM[l-1]+D->NoORF[l-1];
+	}
+	D->SHIFTlmn=D->NoSUM[D->L-1]+D->NoORF[D->L-1];/*create mnSHIFT*/
+return 0;
+}
+
+int fillpara(struct_para *D_para, struct_data *D)
+{
+int l,m,mm;
+	/*initials*/
+	/*K*/
+	for (l=0;l<D->L;l++){
+		for (m=0;m<D->NoORF[l];m++){
+			mm=D->NoSUM[l]+m;
+			D_para->K_lm[mm]=D->y[l*D->M*D->N + m*D->N + D->NoTIME[mm]-1];
+			if(D_para->K_lm[mm]>0){D_para->K_lm[mm]=gsl_sf_log(D_para->K_lm[mm]);}
+		}
+	}
+
+	for (l=0;l<D->L;l++)          {D_para->tau_K_l[l]=1/(0.4*0.4);}                  /*Precision*/
+
+	for (l=0;l<D->L;l++)          {D_para->K_o_l[l]=gsl_sf_log(0.25);}        /*LMean*/
+	D_para->sigma_K_o=1/(0.6*0.6);               /*Precision*/
+	D_para->K_p=gsl_sf_log(0.1);       /*LMean*/
+
+	/*r*/
+	for (l=0;l<D->L;l++){
+		for (m=0;m<D->NoORF[l];m++){
+			mm=D->NoSUM[l]+m;
+			D_para->r_lm[mm]=gsl_sf_log(2.5);
+		}
+	}                          /*LMean*/
+
+	for (l=0;l<D->L;l++)          {D_para->tau_r_l[l]=15;}                  /*Precision*/
+
+	for (l=0;l<D->L;l++)          {D_para->r_o_l[l]=gsl_sf_log(2.5);}        /*LMean*/
+	D_para->sigma_r_o=16;               /*Precision*/
+
+	D_para->r_p=gsl_sf_log(2.5);       /*LMean*/
+
+	/*nu*/
+	for (l=0;l<D->L;l++)          {D_para->nu_l[l]=18;}                      /*LMean*/
+	D_para->sigma_nu=0.0025;   /*Precision for lMean*/
+
+	D_para->nu_p=18;   /*LMean*/
+	/*P*/
+	D_para->P=gsl_sf_log(0.0001);      /*LMean*/
+return 0;
+}
+
+int fillpriors(struct_priors *D_priors)
+{
+	/*Priors*/
+	/*K*/
+	D_priors->sigma_K=1;               D_priors->phi_K=200000;               /*Gamma  Shape; Scale */
+	D_priors->eta_K_p=1;               D_priors->psi_K_o=200000;             /*Gamma  Shape; Scale */
+	/*r*/
+	D_priors->sigma_r=1;               D_priors->phi_r=200000;               /*Gamma  Shape; Scale */
+	D_priors->eta_r_p=1;               D_priors->psi_r_o=200000;             /*Gamma  Shape; Scale */
+	/*nu*/
+	D_priors->eta_nu_p=2;              D_priors->psi_nu=200000;              /*Gamma  Shape; Scale */
+
+	/*K*//*r*//*nu*//*P*/
+	D_priors->K_mu=gsl_sf_log(0.2192928);      D_priors->eta_K_mu=1;      /*Normal  LMean; Precisions */
+	D_priors->r_mu=gsl_sf_log(2.5);            D_priors->eta_r_mu=1;      /*Normal  LMean; Precisions */
+	D_priors->nu_mu=gsl_sf_log(31);            D_priors->eta_nu_mu=1;     /*Normal  LMean; Precisions */
+	D_priors->P_mu=gsl_sf_log(0.0002);         D_priors->eta_P_mu=1/0.01;   /*Normal  LMean; Precisions */
+return 0;
+}
+
+/* eof  */
---
+++ CCode/SHM-g/functions.c
@@ -0,0 +1,142 @@
+#include "headers.h"
+#include "functions.h"
+#include "print.h"
+/*Logistic Growth*/
+
+double logistic_function(double t,double K, double r, double P){
+	double output;
+	output=(K*P*exp(r*t))/(K+P*(exp(r*t)-1));
+	return(output);
+}
+
+double logistic_function_E(double t,double K, double r, double P){
+	double output;
+	output=logistic_function(t,exp(K), exp(r), exp(P));
+	return(output);
+}
+
+/*Gibbs*/
+
+double gauss_sample(gsl_rng *RNG, struct_data *D ,int start, int N,double x[],double tau,double mu_0,double tau_0){
+	double vec,Ndouble,SUM=0;
+	int i;
+	Ndouble=N;
+	for (i=start;i<(start+N);i++){SUM=SUM+x[i];}
+	vec=(tau_0*mu_0+tau*SUM)/(tau_0+Ndouble*tau)+gsl_ran_gaussian(RNG,1/sqrt(tau_0+Ndouble*tau));
+	return(vec);
+}
+
+double gamma_sample(gsl_rng *RNG, struct_data *D ,int start, int N, double x[], double mu,double alpha,double beta){
+	double vec,Ndouble,SUM=0;
+	int i;
+	Ndouble=N;
+	for (i=start;i<(start+N);i++){SUM=SUM+(x[i]-mu)*(x[i]-mu);}
+	vec=gsl_ran_gamma(RNG,alpha+Ndouble/2,1/(1/beta+SUM/2));
+	return(vec);
+}
+
+/*MCMC MH*/
+
+double MCMC_base(gsl_rng *RNG, struct_data *D,struct_para *D_para,struct_priors *D_priors,double *accept,double *h,double para,double (*foo)(struct struct_data *D,struct struct_para *D_para,struct struct_priors *D_priors,double,int,int),int l, int m){
+	double logu,logaprob,can;
+	can=para+gsl_ran_gaussian(RNG,*h);
+	logaprob=(*foo)(D,D_para,D_priors,can,l,m)-(*foo)(D,D_para,D_priors,para,l,m);
+
+	logu=gsl_sf_log(1-gsl_rng_uniform(RNG));
+	if (logaprob>logu){para=can;*accept=*accept+1;}
+	return(para); 
+	}
+
+double MCMC_P(struct_data *D,struct_para *D_para,struct_priors *D_priors,double para,int l, int m){
+	double density,F,SUM=0;
+	int n,mm,nn;
+	for (l=0;l<1;l++){
+		for (m=0;m<D->NoORF[l];m++){
+			mm=D->NoSUM[l]+m;
+			for (n=0;n<D->NoTIME[mm];n++){
+				nn=l*D->M*D->N + m*D->N + n;
+				F=logistic_function_E(D->x[nn],D_para->K_lm[mm],D_para->r_lm[mm],para);
+				SUM=SUM+F*(F-2*D->y[nn])*exp(D_para->nu_l[l]);
+			}
+		}
+	}
+	density=para*( para -2*D_priors->P_mu )*D_priors->eta_P_mu+SUM;
+
+	return(-0.5*density); 
+
+}
+
+double MCMC_nu_l(struct_data *D,struct_para *D_para,struct_priors *D_priors,double para,int l, int m){
+	double density,F,SUM=0;
+	int n,mm,nn;
+	for (m=0;m<D->NoORF[l];m++){
+		mm=D->NoSUM[l]+m;
+		for (n=0;n<D->NoTIME[mm];n++){
+			nn=l*D->M*D->N + m*D->N + n;
+			F=logistic_function_E(D->x[nn],D_para->K_lm[mm],D_para->r_lm[mm],D_para->P);
+			SUM=SUM+(D->y[nn]-F)*(D->y[nn]-F)*exp(para)-para;
+		}
+	}
+	density=para*( para -2*D_para->nu_p )*D_para->sigma_nu+SUM; 
+	return(-0.5*density); 
+}
+
+double MCMC_K_lm(struct_data *D,struct_para *D_para,struct_priors *D_priors,double para,int l, int m){
+	double density,F,SUM=0;
+	int n,mm,nn;
+	mm=D->NoSUM[l]+m;
+	for (n=0;n<D->NoTIME[mm];n++){
+		nn=l*D->M*D->N + m*D->N + n;
+		F=logistic_function_E(D->x[nn],para, D_para->r_lm[mm],D_para->P);
+		SUM=SUM+F*(F-2*D->y[nn])*exp(D_para->nu_l[l]);
+	}	
+	density=para*(para-2*D_para->K_o_l[l])*D_para->tau_K_l[l]+SUM; 
+	return(-0.5*density);
+}
+
+double MCMC_r_lm(struct_data *D,struct_para *D_para,struct_priors *D_priors,double para,int l, int m){
+	double density,F,SUM=0;
+	int n,mm,nn;
+	mm=D->NoSUM[l]+m;
+	for (n=0;n<D->NoTIME[mm];n++){
+		nn=l*D->M*D->N + m*D->N + n;
+		F=logistic_function_E(D->x[nn], D_para->K_lm[mm],para,D_para->P);	
+		SUM=SUM+F*(F-2*D->y[nn])*exp(D_para->nu_l[l]);
+	}	
+	density=para*(para-2*D_para->r_o_l[l])*D_para->tau_r_l[l]+SUM; 
+	return(-0.5*density); 
+}
+
+/*Gibbs and MH steps*/
+
+int gibbsandMHloop(int iter,int thin,gsl_rng *RNG,struct_data *D,struct_para *D_para,struct_priors *D_priors ,struct_MH *D_MH,int print){
+int i,j,l,m,mm;
+	if (print==0){/*printheader(D);*/}	
+	for (i=0;i<iter;i++){
+	for (j=0;j<thin;j++){
+		D_para->P=MCMC_base(RNG,D,D_para,D_priors,&D_MH->accept_P,&D_MH->hP,D_para->P,MCMC_P,-999,-999);
+		D_para->sigma_K_o=gsl_min(200,gamma_sample(RNG,D,0,D->L,D_para->K_o_l,D_para->K_p,D_priors->eta_K_p,D_priors->psi_K_o));
+		D_para->sigma_r_o=gsl_min(200,gamma_sample(RNG,D,0,D->L,D_para->r_o_l,D_para->r_p,D_priors->eta_r_p,D_priors->psi_r_o));
+		D_para->sigma_nu=gamma_sample(RNG,D,0,D->L,D_para->nu_l,D_para->nu_p,D_priors->eta_nu_p,D_priors->psi_nu);	
+		D_para->K_p=gauss_sample(RNG,D,0,D->L,D_para->K_o_l,D_para->sigma_K_o,D_priors->K_mu,D_priors->eta_K_mu);
+		D_para->r_p=gauss_sample(RNG,D,0,D->L,D_para->r_o_l,D_para->sigma_r_o,D_priors->r_mu,D_priors->eta_r_mu);
+		D_para->nu_p=gauss_sample(RNG,D,0,D->L,D_para->nu_l,D_para->sigma_nu,D_priors->nu_mu,D_priors->eta_nu_mu);
+
+		for (l=0;l<D->L;l++){
+			D_para->tau_K_l[l]=gsl_min(2000,gamma_sample(RNG,D,D->NoSUM[l],D->NoORF[l],D_para->K_lm,D_para->K_o_l[l],D_priors->sigma_K,D_priors->phi_K));
+			D_para->tau_r_l[l]=gsl_min(5000,gamma_sample(RNG,D,D->NoSUM[l],D->NoORF[l],D_para->r_lm,D_para->r_o_l[l],D_priors->sigma_r,D_priors->phi_r));
+			D_para->K_o_l[l]=gauss_sample(RNG,D,D->NoSUM[l],D->NoORF[l],D_para->K_lm,D_para->tau_K_l[l],D_para->K_p,D_para->sigma_K_o);
+			D_para->r_o_l[l]=gauss_sample(RNG,D,D->NoSUM[l],D->NoORF[l],D_para->r_lm,D_para->tau_r_l[l],D_para->r_p,D_para->sigma_r_o);
+			D_para->nu_l[l]=MCMC_base(RNG,D,D_para,D_priors,&D_MH->accept_nu,&D_MH->hnu,D_para->nu_l[l],MCMC_nu_l,l,-999);
+			for (m=0;m<D->NoORF[l];m++){ 
+				mm=D->NoSUM[l]+m;
+				D_para->K_lm[mm]=MCMC_base(RNG,D,D_para,D_priors,&D_MH->accept_K,&D_MH->hK,D_para->K_lm[mm],MCMC_K_lm,l,m);
+				D_para->r_lm[mm]=gsl_min(3.5,MCMC_base(RNG,D,D_para,D_priors,&D_MH->accept_r,&D_MH->hr,D_para->r_lm[mm],MCMC_r_lm,l,m));
+			}
+		}
+	}
+	if (print==1){/*printdata(D,D_para,D_MH);*/}
+	}
+return 0;
+}
+
