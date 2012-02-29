e3e373970e4dde1c9194ca5ef24cafd7419e187b
Jonathan <j.j.heydari@ncl.ac.uk>
2012-02-29 14:10:02 +0000

    jh

--- CCode/SHM/shellC.sh
+++ CCode/SHM/shellC.sh
@@ -1,18 +1,21 @@
-#!/bin/bash            
-data="cdc" 
-model="SHM"                                                                                                                                     
+#!/bin/bash           
+                                                              
+step1="`pwd`"
+model_temp="`basename $step1`"
+
 ORF="5000"
 
-burn="1"
+burn="100000"
 iter="1000"
 thin="100"
 
-name="CC_${model}_${data}_${ORF}_${burn}_${iter}_${thin}"
+name="CC_${model_temp}_${ORF}_${burn}_${iter}_${thin}"
 
 echo "#!/bin/sh                                                                                                                                              
-hostname                                                                                                                                                     
-make                                                                                                                                                         
-sleep 10                                                                                                                                                     
+hostname                                                                                                                                                                                                                                                                                          
+                                                                                
+make
+sleep 10
 date                                                                                                                                                         
 chmod 755 main                                                                                                                                               
 ./main $burn $iter $thin $ORF >${name}.R                                                                                                                             
@@ -39,7 +42,7 @@ Output = hello_condor.out
                                                                                                                                                              
 Log = hello_condor.log                                                                                                                                       
                                                                                                                                                              
-transfer_input_files = main,headers.h, Makefile, ydata.txt, xdata.txt, NoTIMEdata.txt, NoORFdata.txt,LMNmaxdata.txt, main.c, functions.c, functions.h, datain.c, datain.h, print.c, print.h                                                                                                                               
+transfer_input_files = main,headers.h, Makefile, priors.txt, ydata.txt, xdata.txt, NoTIMEdata.txt, NoORFdata.txt,LMNmaxdata.txt, main.c, functions.c, functions.h, datain.c, datain.h, print.c, print.h                                                                                                                               
                                                                                                                                                              
 transfer_files = ALWAYS                                                                                                                                      
                                                                                                                                                              
