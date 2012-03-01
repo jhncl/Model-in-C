f530dde8ffe3acbd33b993f1b06383ea49d6f644
Jonathan Heydari <b0919573@cisban-node1.ncl.ac.uk>
2012-03-01 14:30:50 +0000

    jh

--- CCode/JHM/shellC.sh
+++ CCode/JHM/shellC.sh
@@ -1,13 +1,13 @@
 #!/bin/bash                                                                                                                                                  
 step1="`pwd`"
 data="`basename $step1`"
-
-data="cdc"
+step2="`dirname step1`"
+step3="`basename $step2`"
+data=$step3_$data
 model="JHM"
 burn="100000"
 iter="1000"
 thin="100"
-
 
 name="CC_${model}_${data}_${burn}_${iter}_${thin}"
 
