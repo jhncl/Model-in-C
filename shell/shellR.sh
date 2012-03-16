de68f80f31c7760688b7cd8e5240b2857a7999c9
Jonathan <j.j.heydari@ncl.ac.uk>
2012-03-16 13:59:14 +0000

    jh

---
+++ shell/shellR.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+name=`echo $1 | sed '$s/..$//'`
+
+echo "#!/bin/sh
+hostname
+date
+R CMD BATCH $1 R$name.log
+date
+die" > R$name.sh
+
+echo "#####################
+
+# My First Submission
+
+#####################
+
+Executable = R$name.sh
+
+#Arguments = I bring you tidings of joy
+
+Universe = vanilla
+
+#Requirements = OpSys == “LINUX” && Arch ==“X86_64”
+
+Error = hello_condor.err
+
+Output = hello_condor.out
+
+Log = hello_condor.log
+
+transfer_input_files = $1
+
+transfer_files = ALWAYS
+
+Queue
+" > something.classad
+
+chmod 755 R$name.sh
+condor_submit something.classad
+sleep 1
+
+done
